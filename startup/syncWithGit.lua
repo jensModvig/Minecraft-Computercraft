0a2e3323f50784aaa084cc94110779f289add7a8
Jens Modvig <jens.modvig@hotmail.com>
2022-06-12 17:24:27 +0200

    a

--- startup/syncWithGit.lua
+++ startup/syncWithGit.lua
@@ -1,18 +1,19 @@
 local username = "jensModvig"
 local repo = "Minecraft-Computercraft"
-function hte()
-if fs.exists(gitget)
-shell.run("gitget", username, repo)
-else
-print("GitGet application does not exist. Downloading...")
-shell.run("pastebin", "get", "W5ZkVYSi", "gitget")
-shell.run("gitget", username, repo)
-end
-echo("Installed.")
+local gitgetDir = "programs/gitget"
+local function hte()
+    if fs.exists(gitgetDir) then
+        shell.run(gitgetDir, username, repo)
+    else
+        print("GitGet application does not exist. Downloading...")
+        shell.run("pastebin", "get", "W5ZkVYSi", gitgetDir)
+        shell.run(gitgetDir, username, repo)
+    end
+    echo("Installed.")
 end
 if http then
-print("HTTP enabled. You can continue.")
-hte()
+    print("HTTP enabled. You can continue.")
+    hte()
 else
-print("HTTP not enabled. App will not continue.")
+    print("HTTP not enabled. App will not continue.")
 end

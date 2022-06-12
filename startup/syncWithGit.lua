63eadab0a61e4c9eefb58eec8b6b0206879ee189
Jens Modvig <jens.modvig@hotmail.com>
2022-06-12 17:37:59 +0200

    renaming and fixing

--- startup/syncWithGit.lua
+++ startup/syncWithGit.lua
@@ -1,6 +1,6 @@
 local username = "jensModvig"
 local repo = "Minecraft-Computercraft"
-local gitgetDir = "programs/gitget"
+local gitgetDir = "programs/gitget.lua"
 local function hte()
     if fs.exists(gitgetDir) then
         shell.run(gitgetDir, username, repo)
@@ -9,7 +9,7 @@ local function hte()
         shell.run("pastebin", "get", "W5ZkVYSi", gitgetDir)
         shell.run(gitgetDir, username, repo)
     end
-    echo("Installed.")
+    print("Installed.")
 end
 if http then
     print("HTTP enabled. You can continue.")

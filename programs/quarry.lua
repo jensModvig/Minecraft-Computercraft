9ed2822bc042cd383e6823adb461779bdac4a8d7
Jens Modvig <jens.modvig@hotmail.com>
2022-06-16 13:37:09 +0200

    a

--- programs/quarry.lua
+++ programs/quarry.lua
@@ -120,17 +120,17 @@ for j=maxDepth,0,-3 do
             lps.gotoPose(i%2*params.x, j, i)
             lps.gotoPose((i+1)%2*params.x, j, i)
         end
-    else if j % 12 == -3 then
+      else j % 12 == -3 then
         for i=0,params.x-1 do
             lps.gotoPose(i, j, (i+1)%2*params.y)
             lps.gotoPose(i, j, i%2*params.y)
         end
-    else if j % 12 == -6 then
+    else j % 12 == -6 then
         for i=params.y-1,0,-1 do
             lps.gotoPose((i+1)%2*params.x, j, i)
             lps.gotoPose(i%2*params.x, j, i)
         end
-    else if j % 12 == -9 then
+    else j % 12 == -9 then
         for i=params.x-1,0,-1 do
             lps.gotoPose(i, j, i%2*params.y)
             lps.gotoPose(i, j, (i+1)%2*params.y)
